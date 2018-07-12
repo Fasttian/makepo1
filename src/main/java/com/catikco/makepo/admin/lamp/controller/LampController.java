90d0b34e6724968abb6a806967a317a0396b6e23
tianjiaxing <tianjiaxing2017@icloud.com>
2018-07-12 20:34:28 +0800

    产品功能实现中

--- src/main/java/com/catikco/makepo/admin/lamp/controller/LampController.java
+++ src/main/java/com/catikco/makepo/admin/lamp/controller/LampController.java
@@ -1,9 +1,121 @@
 package com.catikco.makepo.admin.lamp.controller;
 
+import com.catikco.makepo.admin.common.DatatablesResponsePageModel;
+import com.catikco.makepo.admin.news.model.NewsEditPageModel;
+import com.catikco.makepo.admin.news.model.NewsListPageModel;
+import com.catikco.makepo.admin.news.model.NewsRequestPageModel;
+import com.catikco.makepo.admin.news.service.NewsService;
+import com.catikco.makepo.oss.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+import static com.catikco.makepo.common.StringUtils.cutContentFileId;
+import static com.catikco.makepo.common.StringUtils.parseContentFileId;
+
 /**
  * Create By: Cai Rong fei @Gui Yang
  * Time: 7/8/2018  14:19
  * Description：
  */
+@Controller
+@RequestMapping
 public class LampController {
+
+    @Autowired
+    private NewsService newsService;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+
+    /**
+     * 后台管理初始页
+     * @return
+     */
+    @RequestMapping("admin-init")
+    public String adminInit(HttpServletResponse response){
+        String  htmlContent = "<h3><img src=\"风景图片IMG_25511530805047509.JPG\" data-filename=\"1\" style=\"width: 974px;\"><img src=\"风景图片IMG_25511530804855509.JPG\" data-filename=\"2\" style=\"width: 974px;\"><img src=\"风景图片IMG_25511530804860863.JPG\" data-filename=\"3\" style=\"width: 974px;\">Hello Summernote</h3>";
+        String fileIds =  parseContentFileId(htmlContent);
+        List<Integer> fidList = cutContentFileId(fileIds);
+        NewsEditPageModel newsEditPageModel = new NewsEditPageModel();
+        newsEditPageModel.setContent(htmlContent);
+        newsEditPageModel.setTitle("新闻测试");
+        newsEditPageModel.setKeywords("关键字测试");
+        newsEditPageModel.setDigest("摘要测试");
+
+        newsService.saveNews(newsEditPageModel,response);
+
+        return "admin/index";
+    }
+
+    /**
+     * 新闻管理页面
+     * @return
+     */
+    @RequestMapping("news-list")
+    public String newsInit(){
+        return "admin/news/news-list";
+    }
+
+    /**
+     * 加载新闻列表
+     * @return
+     */
+    @RequestMapping("load-news-list")
+    @ResponseBody
+    public DatatablesResponsePageModel<NewsListPageModel> loadList(NewsRequestPageModel newsRequestPageModel){
+        return newsService.getNewsList(newsRequestPageModel);
+    }
+
+
+    /**
+     * 初始化新闻编辑/添加页面
+     * @return
+     */
+    @RequestMapping("edit-news")
+    public String edit(Integer id,HttpServletResponse response){
+        return "admin/news/edit-news";
+    }
+
+    /**
+     *
+     * @param newsEditPageModel 编辑页面model
+     * @param multipartFile 文件
+     * @param response 响应页面请求
+     */
+    @RequestMapping("save-news")
+    @ResponseBody
+    public void saveNews(NewsEditPageModel newsEditPageModel, @RequestParam("file")MultipartFile multipartFile, HttpServletResponse response){
+        newsService.saveNews(newsEditPageModel,response);
+    }
+
+
+    /**
+     * 根据id删除新闻
+     * @param id
+     */
+    @RequestMapping("delete-news")
+    public void deleteNews(Integer id){
+
+    }
+
+    /**
+     * 对富文本编辑器中的图片文件执行保存操作
+     * @param multipartFile
+     * @param response
+     */
+    @RequestMapping("/save-file")
+    @ResponseBody
+    public void saveFiles(@RequestParam("file")MultipartFile multipartFile, HttpServletResponse response){
+        fileStorageService.uploads(multipartFile,response);
+    }
+
+
 }
--- src/main/java/com/catikco/makepo/admin/lamp/model/LampEditPageModel.java
+++ src/main/java/com/catikco/makepo/admin/lamp/model/LampEditPageModel.java
@@ -1,9 +1,41 @@
 package com.catikco.makepo.admin.lamp.model;
 
+import java.util.Date;
+
 /**
  * Create By: Cai Rong fei @Gui Yang
  * Time: 7/8/2018  14:20
  * Description：
  */
 public class LampEditPageModel {
+
+    private Integer id;
+
+    private Integer productTitleImageFileid;
+
+    private String model;
+
+    private Double voltage;
+
+    private Double power;
+
+    private Double lumen;
+
+    private String powerFactor;
+
+    private String size;
+
+    private String material;
+
+    private String title;
+
+    private String keywords;
+
+    private String decription;
+
+    private String productUrl;
+
+    private Integer views;
+
+    private Byte productType;
 }
